0bee3b89dc0d2a483b1478a4f07aa676694a0ec7
Androsov Sergei <Androsov-07@mail.ru>
2024-05-03 14:53:16 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите строку: ");
+string? str = Console.ReadLine();
+string[] wordArray = str.Split();
+PrintArray(wordArray);
